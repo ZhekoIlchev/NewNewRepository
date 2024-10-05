76222ec22735fff8525f5bfe0dd1d19ccce63149
Zheko Ilchev <Jeko.Ilchev@gmail.com>
2024-10-06 01:20:58 +0300

    Change content of file 7.

--- HelloWorld/src/File7.java
+++ HelloWorld/src/File7.java
@@ -1,5 +1,5 @@
 public class File7 {
     public static void main(String[] args) {
-        System.out.println("I want to make cherry-pick from HERE :)");
+        System.out.println("Cherry-pick");
     }
 }
